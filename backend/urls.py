f52bbe7df24e04ff8ce28115b70d6d09f2b8a66d
Sameer <itsmdsameerkhan@gmail.com>
2020-05-15 13:29:27 +0545

    Initial Setup with Docker

--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-"""root URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='index.html')),
 ]
